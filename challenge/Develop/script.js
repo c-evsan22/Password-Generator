646d6ec65bf068820f9aa004f1af5d2681ca9280
Chris San <christianes322@gmail.com>
2023-09-28 19:58:21 -0500

    okay trieda new javascript code entry does not work. does not want to link between javascript and html sheet. trying to figue this out next

--- challenge/Develop/script.js
+++ challenge/Develop/script.js
@@ -1,28 +1,24 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+var password = document.querySelector("#generate");
 
 // Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+function generatePassword() {
+  const uppercaseLetters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  const lowercaseLetters = 'abcdefghijklmnopqrstuvwxyz';
+  const numbers = '0123456789';
+  const specialCharacters = '!@#$%^&*()_+~`|}{[]\\:;?><,./-=';
 
-  passwordText.value = password;
+  const allCharacters = uppercaseLetters + lowercaseLetters + numbers + specialCharacters;
 
-}
+  let password = '';
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-
-
-var password=document.getElementById("password");
-
-  function genPassword() {
-    var chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    var passwordLength =12;
-    var password ="";
-for (var i=0; i <= passwordLength; i++) {
-  var randomNumber = Math.floor(Math.random() * chars.length);
-  password += chars.substring(randomNumber, randomNumber +1);
-}
-  document.getElementById("password").value = password;
+  for (let i = 0; i < 12; i++) {
+    const randomIndex = Math.floor(Math.random() * allCharacters.length);
+    password += allCharacters[randomIndex];
   }
+
+  return password;
+}
+
+const password = generatePassword();
+console.log(password);
